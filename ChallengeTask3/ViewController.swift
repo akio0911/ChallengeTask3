205a5b2b3f59d8c641c686a0d2fc2536b777a3cb
ksy781 <k.s.k.s.hanahana@gmail.com>
2020-12-20 01:20:59 +0900

    Fix indent

--- ChallengeTask3/ViewController.swift
+++ ChallengeTask3/ViewController.swift
@@ -7,30 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var textField1: UITextField!
-    @IBOutlet weak var textField2: UITextField!
-    @IBOutlet weak var label1: UILabel!
-    @IBOutlet weak var label2: UILabel!
-    @IBOutlet weak var label3: UILabel!
+// 継承不可
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var label1: UILabel!
+    @IBOutlet private weak var label2: UILabel!
+    @IBOutlet private weak var label3: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // 計算をする
     @IBAction func buttonPressed(_ sender: UIButton) {
         
+        // 入力
         let num1 = Int(textField1.text ?? "") ?? 0
         label1.text = String(num1)
         let num2 = Int(textField2.text ?? "") ?? 0
         label2.text = String(num2)
         
+        // 出力
         var num3 = num1 + num2
         label3.text = String(num3)
+        
     }
+    
 }
 
