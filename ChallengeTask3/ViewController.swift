eabd56b396a4ccbe066e4538d6f5e07ad78cf35a
ksy781 <k.s.k.s.hanahana@gmail.com>
2020-12-20 00:48:16 +0900

    Add input output addition function

--- ChallengeTask3/ViewController.swift
+++ ChallengeTask3/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        let num1 = Int(textField1.text ?? "") ?? 0
+        label1.text = String(num1)
+        let num2 = Int(textField2.text ?? "") ?? 0
+        label2.text = String(num2)
+        
+        var num3 = num1 + num2
+        label3.text = String(num3)
+    }
 }
 
