55cba940a2f2a2abb2f4e4ea47965fcdac8410ca
ksy781 <k.s.k.s.hanahana@gmail.com>
2020-12-20 10:02:34 +0900

    Add switchButtn and description

--- ChallengeTask3/ViewController.swift
+++ ChallengeTask3/ViewController.swift
@@ -15,21 +15,37 @@ final class ViewController: UIViewController {
     @IBOutlet private weak var label1: UILabel!
     @IBOutlet private weak var label2: UILabel!
     @IBOutlet private weak var label3: UILabel!
+    @IBOutlet weak var switch1: UISwitch!
+    @IBOutlet weak var switch2: UISwitch!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        switch1.isOn = false
+        switch2.isOn = false
     }
     
     // 計算をする
     @IBAction func buttonPressed(_ sender: UIButton) {
         
+        print(switch1.isOn)
         // 入力
-        let num1 = Int(textField1.text ?? "") ?? 0
-        label1.text = String(num1)
-        let num2 = Int(textField2.text ?? "") ?? 0
-        label2.text = String(num2)
+        var num1 = Int(textField1.text ?? "") ?? 0
+        if switch1.isOn {
+            num1 = -num1
+            label1.text = String(num1.description)
+        } else {
+            label1.text = String(num1)
+        }
+        
+        var num2 = Int(textField2.text ?? "") ?? 0
+        if switch2.isOn {
+            num2 = -num2
+            label2.text = String(num2.description)
+        } else {
+            label2.text = String(num2)
+        }
         
         // 出力
         var num3 = num1 + num2
